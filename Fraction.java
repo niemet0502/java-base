293dfde2344ea601c214e64264e77b50c6016335
niemet0502 <vincentmarius8@gmail.com>
2021-01-17 13:04:41 +0100

    main :: added constructor content

--- Fraction.java
+++ Fraction.java
@@ -3,14 +3,17 @@ public class Fraction {
   private long denumerator;
 
   public Fraction(){
-
+    this.denumerator = 1;
+    this.numerator = 0;
   }
 
   public Fraction(long numerator, long denumerator){
-    
+    this.denumerator = denumerator;
+    this.numerator = numerator;
   }
-  
+
   public Fraction(long denumerator){
-    
+    this.denumerator = denumerator;
+    this.numerator = 1;
   }
 }
