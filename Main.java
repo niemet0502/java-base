d83407dc896d6f5bf96acc931519c3ea890db441
niemet0502 <vincentmarius8@gmail.com>
2021-01-17 13:01:24 +0100

    main :: created Main class file

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Random rand = new Random();
+        int int_random = rand.nextInt(20);
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Marius NIEMET");
+    }
+}
