004c93a5c3e3cdb0a5662aef62235b3bbb75dd04
niemet0502 <vincentmarius8@gmail.com>
2021-01-17 14:29:21 +0100

    main :: created TestFraction class

---
+++ TestFraction.java
@@ -0,0 +1,10 @@
+public class TestFraction {
+     public static void main(String[] args) {
+        Fraction f2 = new Fraction(4);
+        Fraction f3 = new Fraction(2,3);
+        Fraction f1 = new Fraction();
+        f1 = f2.divide(f3);
+        System.out.println(f1);
+     }    
+
+ }
