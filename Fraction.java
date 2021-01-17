958cfc0071f2d71b99b7f69cac493c45dd45816e
niemet0502 <vincentmarius8@gmail.com>
2021-01-17 13:15:02 +0100

    main :: added setters and getters for Fraction attribute

--- Fraction.java
+++ Fraction.java
@@ -1,19 +1,36 @@
 public class Fraction {
   private long numerator;
-  private long denumerator;
+  private long denominator;
 
   public Fraction(){
-    this.denumerator = 1;
+    this.denominator = 1;
     this.numerator = 0;
   }
 
-  public Fraction(long numerator, long denumerator){
-    this.denumerator = denumerator;
+  public Fraction(long numerator, long denominator){
+    this.denominator = denominator;
     this.numerator = numerator;
   }
 
-  public Fraction(long denumerator){
-    this.denumerator = denumerator;
+  public Fraction(long denominator){
+    this.denominator = denominator;
     this.numerator = 1;
+  }
+  // setters
+  public void setNumerator(long numerator){
+    this.numerator = numerator;
+  }
+
+  public void setDenominator(long denominator){
+    this.denominator = denominator;
+  }
+  
+  //getters
+  public long numerator(){
+    return this.numerator;
+  }
+
+  public long denominator(){
+    return this.denominator;
   }
 }
