f2eaaa8bc42bee6904626f1cf84c7f1e72377d98
niemet0502 <vincentmarius8@gmail.com>
2021-01-17 13:03:07 +0100

    main :: added constructeur

--- Fraction.java
+++ Fraction.java
@@ -2,5 +2,15 @@ public class Fraction {
   private long numerator;
   private long denumerator;
 
+  public Fraction(){
+
+  }
+
+  public Fraction(long numerator, long denumerator){
+    
+  }
   
+  public Fraction(long denumerator){
+    
+  }
 }
